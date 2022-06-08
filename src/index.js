7285f7657c563fd0f6c6b3eaf1135dfa8b575d89
Andrey Ivanov <prioritet2503@gmail.com>
2022-06-08 16:36:14 +0300

    save

--- src/index.js
+++ src/index.js
@@ -10,3 +10,93 @@ const API_KEY = '27913920-68ceae66209fe678afbf6b110';
 const BASE_URL = 'https://pixabay.com/api/?key=';
 const OPTIONS =
   '}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+
+let searchQuery = '';
+let page = 1;
+loadMoreBtn.style.display = 'none';
+let total = 0;
+
+submitForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+  galleryList.innerHTML = '';
+  searchQuery = e.currentTarget.elements.searchQuery.value;
+  total = 0;
+  if (searchQuery !== '') {
+    page = 1;
+    onLoadMore();
+  }
+}
+
+function onLoadMore() {
+  loadMoreBtn.style.display = 'none';
+  getPictures(searchQuery).then(data => {
+    renderPictures(data.hits);
+    loadMoreBtn.style.display = 'block';
+    if (page === 2) {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+  });
+  page += 1;
+}
+
+async function getPictures(searchQuery) {
+  try {
+    const response = await axios.get(
+      BASE_URL + API_KEY + `&q=${searchQuery}` + OPTIONS + `&page=${page}`
+    );
+    total += response.data.hits.length;
+    const totalHits = response.data.totalHits;
+    if (total >= totalHits) {
+      return Notify.warning(
+        'We`re sorry, but you`ve reached the end of search results.'
+      );
+      loadMoreBtn.style.display = 'none';
+    }
+    return response.data;
+  } catch (error) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+}
+
+function renderPictures(pictures) {
+  const markup = pictures
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+      <div class="photo-card">
+      <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" max-width="400px" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b><br>${likes}</br>
+    </p>
+    <p class="info-item">
+      <b>Views</b><br>${views}</br>
+    </p>
+    <p class="info-item">
+      <b>Comments</b><br>${comments}</br>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b><br>${downloads}</br>
+    </p>
+  </div>
+</div>
+      `;
+      }
+    )
+    .join('');
+  galleryList.insertAdjacentHTML('beforeend', markup);
+}
